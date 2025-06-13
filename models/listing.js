91b45751950d4617d23c1cc2a0e72392e33d6ee0
saumya singh <saumyasingh3234@gmail.com>
2025-06-13 13:34:28 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+// const Listing=require("./models/listing.js");
+
+const listingSchema=new Schema({
+   title:{
+    type:String,
+    required:true,
+  },
+   description:String,
+   image:{
+    type:Object,
+    default:"https://plus.unsplash.com/premium_photo-1668024966086-bd66ba04262f?q=80&w=2092&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set:(v)=>v===""?"https://plus.unsplash.com/premium_photo-1668024966086-bd66ba04262f?q=80&w=2092&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" :v,
+  },
+   price:Number,
+   location:String,
+   country:String,
+});
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
